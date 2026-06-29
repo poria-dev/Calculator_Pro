fa7637da0d75f1339d229b02cfa54471f3aad82e
Pooria Rezaee <pooriarezaee.dev@gmail.com>
2026-06-29 14:37:34 +0330

    fix : bug

--- app.js
+++ app.js
@@ -1,60 +1,61 @@
-  // JavaScript code (exactly as you had, untouched)
-    const inp = document.getElementById("inp")
-    const h2 = document.getElementById("h2")
+// JavaScript code (exactly as you had, untouched)
+const inp = document.getElementById("inp")
+const h2 = document.getElementById("h2")
 
-    let number1 = 0
-    let number2 = 0
-    let opa = ""
-
-
-    function addnumber(num) {
-
-        inp.value += num
-
-        if (opa === "") {
-            number1 = +inp.value
-        } else {
-            number2 = +inp.value
-        }
+let number1 = 0
+let number2 = 0
+let opa = ""
 
 
+function addnumber(num) {
+
+    inp.value += num
+
+    if (opa === "") {
+        number1 = +inp.value
+    } else {
+        number2 = +inp.value
     }
 
-    function amal(res) {
 
-        opa = res
+}
 
-        inp.value = ""
+function amal(res) {
+
+    opa = res
+
+    inp.value = ""
+
+}
+
+function sum() {
+
+    if (opa == "+") {
+        h2.innerHTML = number1 + number2
+        
+
+    } else if (opa == "-") {
+
+        h2.innerHTML = number1 - number2
+    } else if (opa == "*") {
+        h2.innerHTML = number1 * number2
+
+    } else {
+        h2.innerHTML = number1 / number2
 
     }
-
-    function sum() {
-
-        if (opa == "+") {
-            h2.innerHTML = number1 + number2
-
-        } else if (opa == "-") {
-
-            h2.innerHTML = number1 - number2
-        } else if (opa == "*") {
-            h2.innerHTML = number1 * number2
-
-        } else {
-            h2.innerHTML = number1 / number2
-
-        }
-        inp.value = ""
+    inp.value = ""
 
 
 
-    }
+}
 
-    function reset() {
-        inp.value = ""
-        number1 = 0
-        number2 = 0
-        opa = ""
-        h2.innerHTML = ""
-    }
+function reset() {
+    inp.value = ""
+    number1 = 0
+    number2 = 0
+    opa = ""
+    h2.innerHTML = ""
+}
 
-    // ---
+// ---
